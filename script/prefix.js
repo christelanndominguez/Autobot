9ebe855a4d85351a041059da3968191dc05de0d7
christelanndominguez <christelanndominguez@gmail.com>
2024-05-27 21:46:43 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -16,7 +16,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
 
     if (!prefix) {
         api.sendMessage(
-            "プレフィックスがありません",
+            "I have No Prefix",
             threadID,
             messageID
         );
