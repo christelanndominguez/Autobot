3981369a8910655dca2e4640340d3f1112f74ff8
christelanndominguez <christelanndominguez@gmail.com>
2024-06-07 11:31:24 +0800

    Update sendnoti.js

--- script/sendnoti.js
+++ script/sendnoti.js
@@ -21,7 +21,7 @@ module.exports.run = async function ({ api, event, args, admin }) {
 	async function sendMessage(thread) {
 		try {
 			await api.sendMessage(
-`🟢🟡🔴\n ----------------\n『 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 』\n\n ----------------\n𝑴𝒆𝒔𝒔𝒂𝒈𝒆 𝒇𝒓𝒐𝒎 𝒂𝒅𝒎𝒊𝒏:「${custom}」\n 𝐀𝐝𝐦𝐢𝐧: Si Christel gud tong gwapa ba\n _________________________`,
+`🟢🟡🔴\n ----------------\n『 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 』\n\n ----------------\n𝑴𝒆𝒔𝒔𝒂𝒈𝒆 𝒇𝒓𝒐𝒎 𝒂𝒅𝒎𝒊𝒏:「${custom}」\n 𝐀𝐝𝐦𝐢𝐧: https://web.facebook.com/profile.php?id=${uid}\n _________________________`,
 				thread.threadID
 			);
 			sentCount++;
